6e0e0b45c2940b99d2317b701c3705a584c84e75
Faan <rudfanno1@gmail.com>
2024-10-01 20:22:19 +0700

    style: login form and navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,35 @@
+import { Flex } from "antd";
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div className="h-12 bg-primary-100 flex justify-center items-center z-50 fixed top-0 w-full">CosBaanDeawGun</div>
-  )
-}
-export default Navbar
+    <Flex className="h-12 bg-primary-100 flex items-center justify-between z-50 fixed top-0 w-full px-12 border-b  drop-shadow-sm">
+      <h1 className="text-xl font-semibold text-primary-600 tracking-wider">
+        CosBaanDeawGun
+      </h1>
+      <Flex>
+        <Link href="/" passHref>
+          <span className="text-primary-600 hover:text-primary-800 ml-6">
+            Home
+          </span>
+        </Link>
+        <Link href="/about" passHref>
+          <span className="text-primary-600 hover:text-primary-800 ml-6">
+            About
+          </span>
+        </Link>
+        <Link href="/contact" passHref>
+          <span className="text-primary-600 hover:text-primary-800 ml-6">
+            Contact
+          </span>
+        </Link>
+        <Link href="/login" passHref>
+          <span className="text-primary-600 hover:text-primary-800 ml-6">
+            Login
+          </span>
+        </Link>
+      </Flex>
+    </Flex>
+  );
+};
+export default Navbar;
--- src/config/theme.ts
+++ src/config/theme.ts
@@ -2,5 +2,16 @@ export const antTheme={
     token:{
         colorPrimary: "#bb763b",
         fontFamily: "Kanit",
+        colorError: "#E35454",
+        colorTextBase: "#653f29",
+        colorLink: "#bb763b",
     }
 }
+
+export const roundedButton = {
+    borderRadius: 24,
+    paddingLeft: 24,
+    paddingRight: 24,
+    paddingTop: 14,
+    paddingBottom: 16,
+};
