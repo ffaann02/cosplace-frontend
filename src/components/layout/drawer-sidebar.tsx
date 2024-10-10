0522f9fc65ac89fc3c28550aba8171e6d01a181f
CowGirl1830 <nawapon.promnan0@gmail.com>
2024-10-10 21:10:55 +0700

    edit navbar

--- src/components/layout/drawer-sidebar.tsx
+++ src/components/layout/drawer-sidebar.tsx
@@ -20,8 +20,33 @@ const DrawerSidebar = ({
       open={open}
       key="left"
     >
-      hello
+      <ul className="flex flex-col gap-4 p-6">
+        <li>
+          <a href="/" className="text-primary-600 hover:text-primary-800">
+            หน้าหลัก
+          </a>
+        </li>
+        <li>
+          <a
+            href="/marketplace"
+            className="text-primary-600 hover:text-primary-800"
+          >
+            Marketplace
+          </a>
+        </li>
+        <li>
+          <a href="/friends" className="text-primary-600 hover:text-primary-800">
+            เพื่อนและสังคม
+          </a>
+        </li>
+        <li>
+          <a href="/events" className="text-primary-600 hover:text-primary-800">
+            กิจกรรมและงาน
+          </a>
+        </li>
+      </ul>
     </Drawer>
   );
 };
+
 export default DrawerSidebar;
