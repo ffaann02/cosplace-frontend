ac893ad2d0d77bca0701ac37438f2faa64834238
Faan <rudfanno1@gmail.com>
2024-11-08 16:44:52 +0700

    fix: unique key on hot topic map render

--- src/app/custom/page.tsx
+++ src/app/custom/page.tsx
@@ -3,15 +3,16 @@ import { PiShootingStar } from "react-icons/pi";
 import CreateCustomPostForm from "@/components/pages/custom/form";
 
 const CustomPost = () => {
-
   return (
     <div className="max-w-7xl mx-auto w-full h-full mt-12 grid grid-cols-5 gap-x-2 lg:gap-x-6 px-4">
-      <div className="divide-y col-span-full md:col-span-4 bg-neutral-50 rounded-xl pt-4 
-        drop-shadow-sm border border-neutral-100">
+      <div
+        className="divide-y col-span-full md:col-span-4 bg-neutral-50 rounded-xl pt-4 
+        drop-shadow-sm border border-neutral-100"
+      >
         <h3 className="text-center mb-3 text-primary-800 font-light">
           เขียนโพสต์จ้างร้านค้า และบริการ
         </h3>
-        <CreateCustomPostForm/>
+        <CreateCustomPostForm />
       </div>
       <div
         className="hidden md:block col-span-1 h-fit border border-neutral-100
@@ -23,7 +24,7 @@ const CustomPost = () => {
         </div>
         <div className="flex flex-col gap-y-2">
           {Array.from({ length: 5 }).map((_, index) => (
-            <div className="b-2">
+            <div className="b-2" key={index}>
               <div className="text-sm lg:text-lg text-primary-600 flex">
                 <p className="font-light">Lorem, ipsum.</p>
                 <FaChevronRight
