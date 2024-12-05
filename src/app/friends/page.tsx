69c8d8aa142511299d66392b1fa3e92c52e78a68
kongkcypw <kongkcypw@gmail.com>
2024-12-05 18:15:36 +0700

    styling: social-page, add: render flex message

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -3,19 +3,24 @@ export const runtime = "edge";
 import SearchPeople from "@/components/pages/friends/search-people";
 import { apiClientWithAuth } from "@/api";
 import { auth } from "@/libs/auth";
-import { Button } from "antd";
+import { Button, Checkbox, Input } from "antd";
 import Link from "next/link";
 import SearchBox from "@/components/pages/friends/matching/search-box";
 import Result from "@/components/pages/friends/matching/result";
 import PersonCard from "@/components/pages/friends/person-card";
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { useAuth } from "@/context/auth-context";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import NoFriendList from "@/components/pages/friends/no-friend-list";
+import FriendCard from "@/components/pages/friends/friend-card";
+import { FaChevronDown, FaSearch } from "react-icons/fa";
+import FriendSearchHeader from "@/components/pages/friends/friend-search-header";
 
 interface SearchParams {
   [key: string]: string | undefined;
 }
 
-interface FriendStructure {
+export interface FriendStructure {
   user_id: string;
   username: string;
   display_name: string;
@@ -37,6 +42,28 @@ const Friends = ({ searchParams }: { searchParams: SearchParams }) => {
   const [cosplayerList, setCosplayerList] = useState<FriendStructure[]>([]);
 
   const { user } = useAuth();
+
+  const scrollSuggestListRef = useRef<HTMLDivElement>(null);
+  const scrollIncomingRequestListRef = useRef<HTMLDivElement>(null);
+  const scrollWaitingAcceptListRef = useRef<HTMLDivElement>(null);
+
+  const [isOverflowing, setIsOverflowing] = useState({
+    suggest: false,
+    incomingRequest: false,
+    waitingAccept: false,
+  });
+
+  const scrollLeft = (scrollRef: RefObject<HTMLDivElement>) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = (scrollRef: RefObject<HTMLDivElement>) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
 
   const GetFriendList = async () => {
     try {
@@ -111,7 +138,40 @@ const Friends = ({ searchParams }: { searchParams: SearchParams }) => {
       GetWaitingAcceptFriend();
       GetCosplayerList();
     }
-  }, [user])
+  }, [user, type])
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (scrollSuggestListRef.current) {
+        const { scrollWidth, clientWidth } = scrollSuggestListRef.current;
+        setIsOverflowing({
+          ...isOverflowing,
+          suggest: scrollWidth > clientWidth,
+        });
+      }
+      else if (scrollIncomingRequestListRef.current) {
+        const { scrollWidth, clientWidth } = scrollIncomingRequestListRef.current;
+        setIsOverflowing({
+          ...isOverflowing,
+          incomingRequest: scrollWidth > clientWidth,
+        });
+      }
+      else if (scrollWaitingAcceptListRef.current) {
+        const { scrollWidth, clientWidth } = scrollWaitingAcceptListRef.current;
+        setIsOverflowing({
+          ...isOverflowing,
+          waitingAccept: scrollWidth > clientWidth,
+        });
+      }
+    };
+
+    // Check on mount and whenever the list changes
+    checkOverflow();
+
+    // Optional: Add resize listener to handle window resizing
+    window.addEventListener('resize', checkOverflow);
+    return () => window.removeEventListener('resize', checkOverflow);
+  }, [incomingRequestFriendList, waitingAcceptFriendList, suggestFriendList]);
 
   return (
     <div className="w-full lg:max-w-[72rem] 2xl:max-w-[80rem] section-container pt-12 md:pt-16 px-4 flex flex-col gap-4">
@@ -120,6 +180,15 @@ const Friends = ({ searchParams }: { searchParams: SearchParams }) => {
           <button
             className={`${type === "list" ? "text-primary-600" : "text-neutral-400"
               } pb-3 border-b-2 ${type === "list" ? "border-b-primary-600" : " border-transparent"
+              } px-4 text-lg transition-all ease-linear duration-200`}
+          >
+            เพื่อนของคุณ
+          </button>
+        </Link>
+        <Link href="/friends?type=request">
+          <button
+            className={`${type === "request" ? "text-primary-600" : "text-neutral-400"
+              } pb-3 border-b-2 ${type === "request" ? "border-b-primary-600" : " border-transparent"
               } px-4 text-lg transition-all ease-linear duration-200`}
           >
             คำขอและแนะนำ
@@ -137,81 +206,157 @@ const Friends = ({ searchParams }: { searchParams: SearchParams }) => {
       </div>
       {type === "list" && (
         <>
-          <h3 className="text-primary-800 mb-2">เพื่อนของคุณ</h3>
-          {friendList.length === 0 && (
-            <p className="text-primary-800 text-left -mt-4">ไม่มี</p>
-          )}
-          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-2 lg:gap-4">
-            {friendList?.map((friend: any, index: number) => (
-              <PersonCard
-                type="friend"
-                key={index}
-                user_id={user?.user_id || ""}
-                username={friend.username}
-                display_name={friend.display_name || friend.first_name}
-                first_name={friend.first_name}
-                last_name={friend.last_name}
-                profile_image_url={friend.profile_image_url}
-              />
-            ))}
+          <div className="bg-primary-50 border border-primary-200 px-4 py-2 rounded-xl text-primary-600 min-h-screen">
+            <FriendSearchHeader />
+            {friendList.length === 0 && (
+              <NoFriendList category="friend mt-4" />
+            )}
+            <div className="grid grid-cols-3 gap-4 mt-4">
+              {friendList?.map((friend: any, index: number) => (
+                <FriendCard
+                  key={index}
+                  user_id={user?.user_id || ""}
+                  username={friend.username}
+                  display_name={friend.display_name || friend.first_name}
+                  first_name={friend.first_name}
+                  last_name={friend.last_name}
+                  profile_image_url={friend.profile_image_url}
+                  getFriendList={GetFriendList}
+                />
+              ))}
+            </div>
           </div>
-
+        </>
+      )}
+      {type === "request" && (
+        <>
           {/* Incoming friend requested */}
-          <h3 className="text-primary-800 mb-2">คำขอเพิ่มเพื่อน</h3>
-          {incomingRequestFriendList.length === 0 && (
-            <p className="text-primary-800 text-left -mt-4">ไม่มี</p>
-          )}
-          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-2 lg:gap-4">
-            {incomingRequestFriendList?.map((friend: any, index: number) => (
-              <PersonCard
-                type="request"
-                key={index}
-                user_id={user?.user_id || ""}
-                username={friend.username}
-                display_name={friend.display_name || friend.first_name}
-                first_name={friend.first_name}
-                last_name={friend.last_name}
-                profile_image_url={friend.profile_image_url}
-              />
-            ))}
+          <div className="relative">
+            <h3 className="text-primary-800 mt-8 mb-2">คำขอเพิ่มเพื่อน</h3>
+            {incomingRequestFriendList.length === 0 && (
+              <NoFriendList category="request" />
+            )}
+            <div className="flex items-center h-full">
+              {/* Card Container */}
+              <div className="grid grid-flow-col auto-cols-max gap-4 overflow-x-auto scrollbar-hide" ref={scrollIncomingRequestListRef}>
+                {incomingRequestFriendList?.map((friend: any, index: number) => (
+                  <PersonCard
+                    type="request"
+                    key={index}
+                    user_id={user?.user_id || ""}
+                    username={friend.username}
+                    display_name={friend.display_name || friend.first_name}
+                    first_name={friend.first_name}
+                    last_name={friend.last_name}
+                    profile_image_url={friend.profile_image_url}
+                  />
+                ))}
+              </div>
+              {/* Scroll button */}
+              {isOverflowing.incomingRequest && (
+                <>
+                  <button
+                    onClick={() => scrollLeft(scrollIncomingRequestListRef)}
+                    className="absolute -left-16 z-10 h-full p-1"
+                  >
+                    <FaAngleLeft className="w-12 h-12 bg-primary-200 text-primary-700 rounded-full p-4" />
+                  </button>
+                  <button
+                    onClick={() => scrollRight(scrollIncomingRequestListRef)}
+                    className="absolute -right-16 z-10 h-full"
+                  >
+                    <FaAngleRight className="w-12 h-12 bg-primary-200 text-primary-700 rounded-full p-4" />
+                  </button>
+                </>
+              )}
+            </div>
           </div>
 
           {/* Waiting accept friend request */}
-          <h3 className="text-primary-800 mb-2">ส่งคำขอเพิ่มเพื่อนแล้ว</h3>
-          {waitingAcceptFriendList.length === 0 && (
-            <p className="text-primary-800 text-left -mt-4">ไม่มี</p>
-          )}
-          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-2 lg:gap-4">
-            {waitingAcceptFriendList?.map((friend: any, index: number) => (
-              <PersonCard
-                type="waiting"
-                key={index}
-                user_id={user?.user_id || ""}
-                username={friend.username}
-                display_name={friend.display_name || friend.first_name}
-                first_name={friend.first_name}
-                last_name={friend.last_name}
-                profile_image_url={friend.profile_image_url}
-              />
-            ))}
+          <div className="relative">
+            <h3 className="text-primary-800 mt-8 mb-2">ส่งคำขอเพิ่มเพื่อนแล้ว</h3>
+            {waitingAcceptFriendList.length === 0 && (
+              <NoFriendList category="waiting" />
+            )}
+            <div className="flex items-center h-full">
+              {/* Card Container */}
+              <div className="grid grid-flow-col auto-cols-max gap-4 overflow-x-auto scrollbar-hide" ref={scrollWaitingAcceptListRef}>
+                {waitingAcceptFriendList?.map((friend: any, index: number) => (
+                  <PersonCard
+                    type="waiting"
+                    key={index}
+                    user_id={user?.user_id || ""}
+                    username={friend.username}
+                    display_name={friend.display_name || friend.first_name}
+                    first_name={friend.first_name}
+                    last_name={friend.last_name}
+                    profile_image_url={friend.profile_image_url}
+                  />
+                ))}
+              </div>
+              {/* Scroll button */}
+              {isOverflowing.waitingAccept && (
+                <>
+                  <button
+                    onClick={() => scrollLeft(scrollWaitingAcceptListRef)}
+                    className="absolute -left-16 z-10 h-full p-1"
+                  >
+                    <FaAngleLeft className="w-12 h-12 bg-primary-200 text-primary-700 rounded-full p-4" />
+                  </button>
+                  <button
+                    onClick={() => scrollRight(scrollWaitingAcceptListRef)}
+                    className="absolute -right-16 z-10 h-full"
+                  >
+                    <FaAngleRight className="w-12 h-12 bg-primary-200 text-primary-700 rounded-full p-4" />
+                  </button>
+                </>
+              )}
+            </div>
           </div>
 
           {/* Suggest friend */}
-          <h3 className="text-primary-800 mt-16 mb-2">คนที่คุณอาจรู้จัก</h3>
-          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-2 lg:gap-4">
-            {suggestFriendList?.map((suggestion: any, index: number) => (
-              <PersonCard
-                type="suggest"
-                key={index}
-                user_id={user?.user_id || ""}
-                username={suggestion.username}
-                display_name={suggestion.display_name || suggestion.first_name}
-                first_name={suggestion.first_name}
-                last_name={suggestion.last_name}
-                profile_image_url={suggestion.profile_image_url}
-                ignoreSuggestion={() => setSuggestFriendList(suggestFriendList.filter((_, i) => i !== index))}
-              />
-            ))}
+          <div className="relative">
+            <h3 className="text-primary-800 mt-8 mb-2">คนที่คุณอาจรู้จัก</h3>
+            {suggestFriendList.length === 0 && (
+              <NoFriendList category="suggest" />
+            )}
+            <div className="flex items-center h-full">
+              {/* Card Container */}
+              <div className="grid grid-flow-col auto-cols-max gap-4 overflow-x-auto scrollbar-hide" ref={scrollSuggestListRef}>
+                {suggestFriendList?.map((suggestion: any, index: number) => (
+                  <PersonCard
+                    type="suggest"
+                    key={index}
+                    user_id={user?.user_id || ""}
+                    username={suggestion.username}
+                    display_name={suggestion.display_name || suggestion.first_name}
+                    first_name={suggestion.first_name}
+                    last_name={suggestion.last_name}
+                    profile_image_url={suggestion.profile_image_url}
+                    ignoreSuggestion={() =>
+                      setSuggestFriendList(suggestFriendList.filter((_, i) => i !== index))
+                    }
+                  />
+                ))}
+              </div>
+              {/* Scroll button */}
+              {isOverflowing.suggest && (
+                <>
+                  <button
+                    onClick={() => scrollLeft(scrollSuggestListRef)}
+                    className="absolute -left-16 z-10 h-full p-1"
+                  >
+                    <FaAngleLeft className="w-12 h-12 bg-primary-200 text-primary-700 rounded-full p-4" />
+                  </button>
+                  <button
+                    onClick={() => scrollRight(scrollSuggestListRef)}
+                    className="absolute -right-16 z-10 h-full"
+                  >
+                    <FaAngleRight className="w-12 h-12 bg-primary-200 text-primary-700 rounded-full p-4" />
+                  </button>
+                </>
+              )}
+            </div>
           </div>
 
         </>
@@ -219,7 +364,7 @@ const Friends = ({ searchParams }: { searchParams: SearchParams }) => {
       {type === "matching" && (
         <>
           <SearchBox />
-          <Result cosplayerList={cosplayerList}/>
+          <Result cosplayerList={cosplayerList} />
         </>
       )}
     </div>
---
+++ src/components/popup/chat/flex-message.tsx
@@ -0,0 +1,97 @@
+import { Image } from 'antd';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+import { IoOpenOutline } from "react-icons/io5";
+
+const FlexMessage = ({
+    side,
+    message,
+    image,
+    dateTime,
+}: {
+    side: string;
+    message: {
+        text: string;
+        image_url: string
+        header: string;
+        external_url: string;
+    };
+    image: string;
+    dateTime: {
+        year: number;
+        month: number;
+        day: number;
+        hour: number;
+        minute: number;
+        second: number;
+    };
+}) => {
+
+    const router = useRouter();
+
+    const convertToThaiMonth = (month: number) => {
+        const monthArray = [
+            "มกราคม",
+            "กุมภาพันธ์",
+            "มีนาคม",
+            "เมษายน",
+            "พฤษภาคม",
+            "มิถุนายน",
+            "กรกฎาคม",
+            "สิงหาคม",
+            "กันยายน",
+            "ตุลาคม",
+            "พฤศจิกายน",
+            "ธันวาคม",
+        ];
+        return monthArray[month - 1];
+    }
+
+    const handleNavigateButton = () => {
+        router.push("/");
+    }
+
+    // Flex message => message with image
+    // Flex message + icon => link to source (absolute)
+    // Image
+
+    return (
+        <div className='w-full h-full'>
+            <div className={`flex ${side === "receiver" ? "justify-start " : "justify-end "} `}>
+                <div className={`relative w-[60%] rounded-lg ${side === "receiver" ? "ml-2 bg-neutral-100 text-neutral-600" : "mr-2 bg-primary-50 text-primary-800"} `}>
+                    {/* External link button */}
+                    {message.external_url && (
+                        <div className='absolute w-full h-full z-10'>
+                            <div className='top-2 right-2'>
+                                <button className='bg-white' onClick={handleNavigateButton}>
+                                    <IoOpenOutline className='text-primary-800 text-lg' />
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                    <Image
+                        className="object-cover w-full rounded-t-lg my-auto mx-auto"
+                        src={image}
+                        alt="Message with image"
+                        height={150}
+                        onClick={() => console.log("click")}
+                        preview={false}
+                    />
+                    <div className='mx-2'>
+                        {/* Header */}
+                        {message.header && <h3 className="text-left text-sm font-medium">{message.header}</h3>}
+                        {/* Text */}
+                        {message.text && <h3 className="text-left text-xs">{message.text}</h3>}
+                        {/* Date Time */}
+                        <div className={`border-t mt-1 ${side === "receiver" ? "border-neutral-400" : "border-primary-400"}`}></div>
+                        <div className="text-left text-xs my-2 font-thin">
+                            {dateTime.day} {convertToThaiMonth(dateTime.month)} {dateTime.hour}:{dateTime.minute}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FlexMessage
